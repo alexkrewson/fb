86f4077a1505775aa3f9e7533c0be8fb8e7e0e52
alexkrewson <alex.krewson@gmail.com>
2021-01-20 11:22:53 -0800

    creates posts resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: 'humblebragger@humblebrag.com', password: 'humblebaby')
+User.create(email: 'fitnessgrampacer@test.com', password: 'pacertest')
+User.create(email: 'a@a.com', password: 'aaaaaa')
+
+Post.create(body: 'Look at me I am a post body!', user_id: '3')
